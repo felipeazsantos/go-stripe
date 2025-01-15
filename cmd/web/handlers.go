1a01ba81a3e8dbc9631713f918a488d2802df40f
Felipe Azevedo <felipe.azevedo@cws.digital>
2025-01-15 18:32:30 -0300

    building form to comunicate with stripe

--- cmd/web/handlers.go
+++ cmd/web/handlers.go
@@ -5,6 +5,5 @@ import "net/http"
 func (app *application) VirtualTerminal(w http.ResponseWriter, r *http.Request) {
 	if err := app.renderTemplate(w, r, "terminal", nil); err != nil {
 		app.errorLog.Println(err)
-
 	}
 }
